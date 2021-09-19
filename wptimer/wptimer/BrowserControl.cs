b4374d5b5ada868e265b23f5b0adace941e1a095
kgnybr <83376821+kgnybr@users.noreply.github.com>
2021-09-20 01:39:45 +0300

    update

--- wptimer/wptimer/BrowserControl.cs
+++ wptimer/wptimer/BrowserControl.cs
@@ -12,18 +12,19 @@ namespace wptimer
     
     class BrowserControl
     {
-        //Browserların ve browserlarda whatsapp webin açılmasını sağlayan methodlar.
+        //Browserların ve browserlarda whatsapp webin açılmasını sağlayan methodlar. 
         public void OOpera()
         {
             string userName = Environment.UserName;
             
-            Process.Start($"C:\\Users\\{userName}\\AppData\\Local\\Programs\\Opera\\launcher.exe");
-            Thread.Sleep(1000);
-            SendKeys.Send("^" + "t");
-            SendKeys.Send("http://web.whatsapp.com");
-            SendKeys.Send("{ENTER}");
+            Process.Start($"C:\\Users\\{userName}\\AppData\\Local\\Programs\\Opera\\launcher.exe"); //Browserın konumunu belirtir. Eğer windows kullanıcı adı daha önceden değiştirildiyse Opera çalıştırırken sorun çıkacaktır(yaşandı:))).
+            Thread.Sleep(1000);//Browserin açılması için 1sn bekler
+            SendKeys.Send("^" + "t");//browser açıldığında eski sekmelere müdahale etmemek için Ctrl+t tuşlarına basarak yeni sekme açar.
+            SendKeys.Send("http://web.whatsapp.com"); //Adres çubuğuna whatsapp web'in adresine girer.
+            SendKeys.Send("{ENTER}"); //Girilen adrese gitmek için "Enter" tuşuna basar.
             
         }
+        //Diğer kodlar diğer browserlar için aynı işlemleri yapar. Hepsi için ayrı anlatmaya gerek duymuyorum.
         public void OChrome()
         {
             
--- wptimer/wptimer/Form1.cs
+++ wptimer/wptimer/Form1.cs
@@ -40,32 +40,33 @@ namespace wptimer
         //Browser açıldıktan ve wp webe giriş yapıldıktan sonra mesaj gönderimini sağlayan method
         public void sendMsg()
         {
-            Thread.Sleep(wpsleep);
-            foreach (var currentNum in numList.Items)
+            Thread.Sleep(wpsleep); //Whatsappın açılması için wpsleep değişkenindeki değer kadar bekler. Garanti olması için değeri 35sn olarak belirledim.
+            foreach (var currentNum in numList.Items) //Her bir numara için mesaj gönderme işlemini yanpan döngü.
             {
                 
-                SendKeys.Send("{TAB}");
-                SendKeys.Send(Convert.ToString(currentNum));
-                SendKeys.Send("{ENTER}");
-                Thread.Sleep(500);
-                SendKeys.Send(msgBox.Text);
-                SendKeys.Send("{ENTER}");
-                SendKeys.Send("{TAB}");
+                SendKeys.Send("{TAB}"); //Arama kutucuğuna gelmek için tab tuşuna basar.
+                SendKeys.Send(Convert.ToString(currentNum)); //currentnum yani işlemdeki numarayı yazar.
+                SendKeys.Send("{ENTER}"); // enter tuşuna basarak yazdığı numara ait konuşmaya gider.
+                Thread.Sleep(500);//konuşmaya girmesi için yarım saniye bekler
+                SendKeys.Send(msgBox.Text); //Mesaj kutusundaki yazıyı yazar.
+                SendKeys.Send("{ENTER}"); //Enter tuşuna basar ve mesajı gönderir.
+                SendKeys.Send("{TAB}");//Bir sonraki numaraya hazırlanmak için tekrar tab tuşuna basar.
             }
-            timer1.Enabled = false;
-            MessageBox.Show("İşlemler tamamlandı", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            timer1.Enabled = false; // İşlem bir dakikadan kısa sürerse ve timer aktif olursa aynı mesajları tekrar gönderecektir bunu engellemek içni timerı pasif hale getiriyorum.
+            MessageBox.Show("İşlemler tamamlandı", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information); // Mesajlar gönderildikten sonra kullanıcıya haber verir.
         }
 
-        private void startButton_Click(object sender, EventArgs e)
+        private void startButton_Click(object sender, EventArgs e) //Başlat butonu
         {
-            if (numList.Items.Count > 0 && msgBox.Text.Length > 0 && stBox.Text.Length > 4 )
+            if (numList.Items.Count > 0 && msgBox.Text.Length > 0 && stBox.Text.Length > 4 ) //Gerekli kısımların gerekli şekilde doldurulup doldurulmadığını kontrol eder.
             {
-                if ( radioC.Checked == true || radioF.Checked == true || radioO.Checked == true || gxRadio.Checked == true) {
+                if ( radioC.Checked == true || radioF.Checked == true || radioO.Checked == true || gxRadio.Checked == true) // Browserların seçilip seçilmediğini kontrol eder. Seçili ise timer aktif olur ve tarih'i kontrol etmeye başlar.
+                {
                     timer1.Enabled = true;
                 }
                 else
                 {
-                    MessageBox.Show("Eksik giriş", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Eksik giriş", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error); //Eksik giriş olursa hata mesajı verir.
                 }
             }
             else
@@ -77,28 +78,28 @@ namespace wptimer
 
         private void addNum_Click(object sender, EventArgs e)
         {
-            if (numBox.TextLength > 9)
+            if (numBox.TextLength > 9) //Girilen numaranın uzunluğunun yeterli olup olmadığını kontrol eder.
             {
-                numList.Items.Add(numBox.Text);
+                numList.Items.Add(numBox.Text); //Numarayı listeye ekler.
             }
             else
             {
-                MessageBox.Show("Numara EN AZ 10 Haneli Olmalıdır", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Numara EN AZ 10 Haneli Olmalıdır", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error); //Uzunluk yeterli olmadığı için hata verir.
             }
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e) //Her 100ms'de bir tarih ve saatiin seçilen tarih ve saatle eşleşip eşleşmediğini kontrol eder. Eşleşirse gerekli methodları çağırır.
         {
-            currentTime = DateTime.Now.ToString("HH:mm");
-            selDate = dateTimePicker1.Value.ToString("dd/MM/yyyy") ; 
-            selTime = stBox.Text;
-            currentDate = DateTime.Now.ToString("dd/MM/yyyy");
-            dateLbl.Text = "Sistem Zamanı: " + currentDate + " " + currentTime;
-            selLbl.Text = "Mesaj Zamanı: " + selDate + " " + selTime;    
-            if(currentDate == selDate && currentTime==selTime)
+            currentTime = DateTime.Now.ToString("HH:mm"); // anlık saati currentTime değişkenine yazar.
+            selDate = dateTimePicker1.Value.ToString("dd/MM/yyyy") ; //seçilmiş tarihi selDate değişkenine yazar.
+            selTime = stBox.Text; // seçilmiş saati selTime değişkenine yazar.
+            currentDate = DateTime.Now.ToString("dd/MM/yyyy"); // anlık tarihi currentDate değişkenine yazar.
+            dateLbl.Text = "Sistem Zamanı: " + currentDate + " " + currentTime; //Bilgi amaçlı dateLbl isimli label'e sistem zamanını yazar.
+            selLbl.Text = "Mesaj Zamanı: " + selDate + " " + selTime;    //Bigli amaçlı seçilmiş zamanı selLbl isimli label'e yazar.
+            if(currentDate == selDate && currentTime==selTime)//Seçilen zamanla anlık zamanın eşleşip eşleşmediğini kontrol eder.
             {
                 
-                    if (radioC.Checked == true)
+                    if (radioC.Checked == true) //Hangi browserların seçili olduğunu kontrol edip buna göre işlem yapan if blokları.
                     {
                         browserControl.OChrome();
                         sendMsg();
@@ -125,13 +126,15 @@ namespace wptimer
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //Excel'den veri alınmasını sağlayan buton.
         {
             string sFileName;
-            OpenFileDialog tpsofdlog = new OpenFileDialog();
+            OpenFileDialog tpsofdlog = new OpenFileDialog(); //Dosya seçme penceresi açar.
             if (tpsofdlog.ShowDialog() == DialogResult.OK)
             {
+                
                  sFileName = tpsofdlog.FileName;
+                //Bu kısımda biraz google'den yardım aldım fazla açıklayıcı olamayacağım :).
                 con = new OleDbConnection($@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {sFileName}; Extended Properties ='Excel 12.0 xml; HDR = YES; '");
                 cmd = new OleDbCommand("SELECT *FROM [nums$]", con);
                 con.Open();
@@ -149,24 +152,24 @@ namespace wptimer
             
         }
 
-        private void deleteNum_Click(object sender, EventArgs e)
+        private void deleteNum_Click(object sender, EventArgs e) //Listeden seçili olan numarayı silmeye yarayan buton.
         {
             numList.Items.Remove(numList.SelectedItem);
         }
 
    
 
-        private void label1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e) //Tasarım için birkaç doknuş (biliyorum tasarımı pek hoş değil idare edin). Programı kapatmaya yarar.
         {
             this.Close();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)//Programı aşağıya almaya yarar.
         {
             this.WindowState = FormWindowState.Minimized;
         }
 
-        private void label1_MouseEnter(object sender, EventArgs e)
+        private void label1_MouseEnter(object sender, EventArgs e) //Geri kalan kısım tasarım için ufak dokunuşlar fazla önemli olmadığı için üzerinde durmayacağım.
         {
             exitLbl.ForeColor = Color.Gray;
         }
@@ -207,3 +210,5 @@ namespace wptimer
         }
     }
 }
+//Umarım beğenirsiniz.
+//Kağan Aybar
